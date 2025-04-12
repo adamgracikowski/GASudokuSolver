5a786b985d8984d325534875ad711d4badf972d4
Adam Grącikowski <01180781@pw.edu.pl>
2025-04-12 23:01:26 +0200

    add styling to the sudoku board

---
+++ GASudokuSolver/GASudokuSolver.GUI/Converters/SudokuBorderConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GASudokuSolver.GUI.Converters;
+
+public sealed class SudokuBorderConverter : IValueConverter
+{
+	public const double BorderSlim = 0.4;
+	public const double BorderThick = 1.2;
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is SudokuCell cell)
+		{
+			var top = ThickOrSlim(cell.Row);
+			var left = ThickOrSlim(cell.Column);
+			var right = ThickOrSlim(cell.Column + 1);
+			var bottom = ThickOrSlim(cell.Row + 1);
+
+			return new Thickness(left, top, right, bottom);
+		}
+
+		return new Thickness(0.5);
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+
+	private static double ThickOrSlim(int index)
+	{
+		return index % 3 == 0 ? BorderThick : BorderSlim;
+	}
+}
--- GASudokuSolver/GASudokuSolver.GUI/MainWindow.xaml.cs
+++ GASudokuSolver/GASudokuSolver.GUI/MainWindow.xaml.cs
@@ -7,13 +7,32 @@ public partial class MainWindow : Window
 {
 	public Func<double, string> AxisLabelFormatter { get; set; } = value => value.ToString("N2");
 
-	public ObservableCollection<int> SudokuCells { get; set; } = new([.. Enumerable.Range(1, 81).Select(_ => Random.Shared.Next(1, 9))]);
+	public ObservableCollection<SudokuCell> SudokuCells { get; set; } = [];
+
+	public void InitializeBoard()
+	{
+		SudokuCells = [.. Enumerable.Range(0, 81).Select(i => new SudokuCell
+			{
+				Row = i / 9,
+				Column = i % 9,
+				Value = Random.Shared.Next(1, 9)
+			})];
+	}
+
 
 
 	public MainWindow()
 	{
 		InitializeComponent();
+		InitializeBoard();
 
 		DataContext = this;
 	}
+}
+
+public class SudokuCell
+{
+	public int Row { get; set; }
+	public int Column { get; set; }
+	public int? Value { get; set; }
 }
