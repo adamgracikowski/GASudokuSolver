6aa0e08b9dc2607afbef174589f2185b95b5ea03
Adam Grącikowski <01180781@pw.edu.pl>
2025-04-12 22:41:24 +0200

    add gui project and basic layout

---
+++ GASudokuSolver/GASudokuSolver.GUI/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace GASudokuSolver.GUI;
+
+public partial class MainWindow : Window
+{
+	public Func<double, string> AxisLabelFormatter { get; set; } = value => value.ToString("N2");
+
+	public ObservableCollection<int> SudokuCells { get; set; } = new([.. Enumerable.Range(1, 81).Select(_ => Random.Shared.Next(1, 9))]);
+
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		DataContext = this;
+	}
+}
