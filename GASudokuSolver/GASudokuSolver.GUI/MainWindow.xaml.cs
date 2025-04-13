eeedad45ff69c792b26ae4963916d312953fd90d
Adam Grącikowski <01180781@pw.edu.pl>
2025-04-13 09:38:49 +0200

    design algorithm results and add mock class

--- GASudokuSolver/GASudokuSolver.GUI/MainWindow.xaml.cs
+++ GASudokuSolver/GASudokuSolver.GUI/MainWindow.xaml.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections.ObjectModel;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace GASudokuSolver.GUI;
 
 public partial class MainWindow : Window
 {
+	public SeriesCollection FitnessSeries { get; set; }
+	public ChartValues<double> FittnessValues { get; set; } = new();
+
+	public double CurrentFitness { get; set; }
+	public int CurrentGeneration { get; set; }
+	public ObservableCollection<string> AlgorithmResults { get; set; } = new();
+
 	public Func<double, string> AxisLabelFormatter { get; set; } = value => value.ToString("N2");
 
 	public ObservableCollection<SudokuCell> SudokuCells { get; set; } = [];
@@ -15,7 +24,7 @@ public partial class MainWindow : Window
 			{
 				Row = i / 9,
 				Column = i % 9,
-				Value = Random.Shared.Next(1, 9)
+				Value = Random.Shared.Next(0, 9)
 			})];
 	}
 
@@ -23,10 +32,26 @@ public partial class MainWindow : Window
 
 	public MainWindow()
 	{
+
 		InitializeComponent();
 		InitializeBoard();
 
+		FitnessSeries =
+		[
+			new LineSeries
+			{
+				Title = "Fitness",
+				Values = FittnessValues,
+				PointGeometry = null
+			}
+		];
+
 		DataContext = this;
+	}
+
+	private void StartButtonClick(object sender, RoutedEventArgs e)
+	{
+		AlgorithmResultsGrid.Visibility = Visibility.Visible;
 	}
 }
 
@@ -35,4 +60,43 @@ public class SudokuCell
 	public int Row { get; set; }
 	public int Column { get; set; }
 	public int? Value { get; set; }
+}
+
+public sealed record AlgorithmProgressData(
+	double FitnessValue, 
+	int[,] Board, 
+	int Generation
+);
+
+public sealed class AlgorithmMock
+{
+	public async Task<AlgorithmProgressData> Run(int[,] board, IProgress<AlgorithmProgressData>? progress = null) 
+	{
+		var generations = 300;
+
+		var empty = new List<(int r, int c)>();
+
+		for(var r = 0; r < board.GetLength(0); r++)
+		{
+			for(var c = 0; c < board.GetLength(1); c++)
+			{
+				if (board[r,c] == 0)
+					empty.Add((r, c));
+			}
+		}
+
+		for(var i = 0; i < generations; i++)
+		{
+			foreach(var (r, c) in empty)
+			{
+				board[r, c] = Random.Shared.Next(1, 9);
+			}
+
+			await Task.Delay(200);
+
+			progress?.Report(new AlgorithmProgressData(10 * Random.Shared.NextDouble() + 1, board, i));
+		}
+
+		return new AlgorithmProgressData(10 * Random.Shared.NextDouble() + 1, board, generations);
+	}
 }
