12ac073d685d37fd165229085228101891ccf82d
Adam Grącikowski <01180781@pw.edu.pl>
2025-04-13 22:32:39 +0200

    refactor converters, styles and main menu

---
+++ GASudokuSolver/GASudokuSolver.GUI/Controls/MainMenu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GASudokuSolver.GUI.Controls;
+
+public partial class MainMenu : UserControl
+{
+	public event RoutedEventHandler SaveBoardClicked;
+
+	public event RoutedEventHandler LoadBoardClicked;
+	
+	public event RoutedEventHandler ExitClicked;
+	
+	public event RoutedEventHandler AboutClicked;
+	
+	public event RoutedEventHandler UserGuideClicked;
+
+#pragma warning disable CS8618
+	public MainMenu()
+#pragma warning restore CS8618
+	{
+		InitializeComponent();
+	}
+
+	private void SaveBoardCsvClick(object sender, RoutedEventArgs e) => SaveBoardClicked?.Invoke(sender, e);
+	
+	private void LoadBoardCsvClickAsync(object sender, RoutedEventArgs e) => LoadBoardClicked?.Invoke(sender, e);
+	
+	private void ExitClick(object sender, RoutedEventArgs e) => ExitClicked?.Invoke(sender, e);
+	
+	private void AboutClick(object sender, RoutedEventArgs e) => AboutClicked?.Invoke(sender, e);
+	
+	private void UserGuideClick(object sender, RoutedEventArgs e) => UserGuideClicked?.Invoke(sender, e);
+}
