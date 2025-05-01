35788f2e3418fdfc5d002aa335948005d38f57e0
Adam Grącikowski <01180781@pw.edu.pl>
2025-05-01 21:00:50 +0200

    add mutations

---
+++ GASudokuSolver/GASudokuSolver.Core/Solver/Mutations/NeighbourStepMutation.cs
@@ -0,0 +1,21 @@
+﻿using GASudokuSolver.Core.Solver.Genes;
+using GASudokuSolver.Core.Solver.Interfaces;
+
+namespace GASudokuSolver.Core.Solver.Mutations;
+
+public sealed class NeighbourStepMutation : IMutation
+{
+	public void Mutate(Gene gene)
+	{
+		if (gene is not CandidateChoiceGene c)
+			return;
+
+		var length = c.Candidates.Length;
+
+		if (length <= 1) return;
+
+		var delta = Random.Shared.NextDouble() < 0.5 ? -1 : +1;
+		
+		c.Index = (c.Index + delta + length) % length;
+	}
+}
--- GASudokuSolver/GASudokuSolver.Core/Solver/Mutations/PercentChanceMutation.cs
+++ GASudokuSolver/GASudokuSolver.Core/Solver/Mutations/PercentChanceMutation.cs
@@ -3,19 +3,20 @@ using GASudokuSolver.Core.Solver.Interfaces;
 
 namespace GASudokuSolver.Core.Solver.Mutations;
 
-public class PercentChanceMutation : IMutation
+public sealed class PercentChanceMutation : IMutation
 {
-	private double percentage;
+	private readonly double percentage;
 
 	public PercentChanceMutation(double chance)
 	{
 		chance = Math.Clamp(chance, 0, 100);
-		this.percentage = chance/100;
+		this.percentage = chance / 100;
 	}
 
 	public void Mutate(Gene gene)
 	{
-		double random = Random.Shared.NextDouble();
+		var random = Random.Shared.NextDouble();
+		
 		if(random <= percentage)
 		{
 			gene.Mutate();
