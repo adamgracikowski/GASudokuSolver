91a1231415b905835cb8d7d57ee78254b2fe23bd
Adam Grącikowski <01180781@pw.edu.pl>
2025-05-02 14:38:41 +0200

    design population settings tab initial version

---
+++ GASudokuSolver/GASudokuSolver.GUI/Controls/PopulationSettingsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GASudokuSolver.GUI.Controls.ViewModels;
+using System.Windows.Controls;
+
+namespace GASudokuSolver.GUI.Controls;
+
+public partial class PopulationSettingsControl : UserControl
+{
+	public PopulationSettingsControl()
+	{
+		InitializeComponent();
+	}
+
+	public PopulationSettingsViewModel ViewModel
+		=> (PopulationSettingsViewModel)DataContext;
+}
---
+++ GASudokuSolver/GASudokuSolver.GUI/Controls/ViewModels/PopulationSettingsViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GASudokuSolver.GUI.Controls.ViewModels;
+
+public class PopulationSettingsViewModel : INotifyPropertyChanged
+{
+	private int populationSize = DefaultPopulationSize;
+	private int numberOfParents = DefaultNumberOfParents;
+	private int maxGenerations = DefaultMaxGenerations;
+	private TimeSpan maxTime = DefaultMaxTime;
+
+	public const int DefaultPopulationSize = 100;
+	public const int DefaultNumberOfParents = 2;
+	public const int DefaultMaxGenerations = 1000;
+	public static readonly TimeSpan DefaultMaxTime = TimeSpan.FromSeconds(30);
+
+	public int PopulationSize
+	{
+		get => populationSize;
+		set
+		{
+			var v = Math.Max(1, value);
+			if (populationSize != v)
+			{
+				populationSize = v;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public int NumberOfParents
+	{
+		get => numberOfParents;
+		set
+		{
+			// cannot exceed population size
+			var v = Math.Clamp(value, 1, PopulationSize);
+			if (numberOfParents != v)
+			{
+				numberOfParents = v;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public int MaxGenerations
+	{
+		get => maxGenerations;
+		set
+		{
+			var v = Math.Max(1, value);
+			if (maxGenerations != v)
+			{
+				maxGenerations = v;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public double MaxTimeSeconds
+	{
+		get => MaxTime.TotalSeconds;
+		set
+		{
+			var secs = Math.Max(0, value);
+			var ts = TimeSpan.FromSeconds(secs);
+			if (ts != MaxTime)
+			{
+				MaxTime = ts;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(MaxTime));
+			}
+		}
+	}
+
+	public TimeSpan MaxTime
+	{
+		get => maxTime;
+		set
+		{
+			var v = value.TotalMilliseconds < 0
+				? DefaultMaxTime
+				: value;
+			if (maxTime != v)
+			{
+				maxTime = v;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	protected void OnPropertyChanged([CallerMemberName] string? name = null)
+		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
